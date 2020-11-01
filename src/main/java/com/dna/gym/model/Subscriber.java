920226a2470411a4dac2b31fd0ebca21eda6b9d4
yazan alkhateeb <yazan@beamwallet.me>
2020-11-01 09:11:39 +0200

    add subscriber found by subscriber id

--- src/main/java/com/dna/gym/model/Subscriber.java
+++ src/main/java/com/dna/gym/model/Subscriber.java
@@ -8,7 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.io.File;
 import java.time.LocalDate;
 
 @Entity
@@ -19,8 +22,13 @@ public class Subscriber {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
     private String subscriberId;
-	private LocalDate startOfSubscription;
+    private LocalDate startOfSubscription;
     private LocalDate endOfSubscription;
-
+    @Lob
+    @Column(length = 1000)
+    private String image;
 }
